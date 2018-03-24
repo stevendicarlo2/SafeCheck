1e0fab080bcbb52147b262cf90de8531abf49601
Ethan hodges <elh5wc@virginia.edu>
2018-03-24 16:18:44 -0400

    Facebook supports Putin

--- SafeCheck/App.js
+++ SafeCheck/App.js
@@ -1,13 +1,39 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import { StackNavigator } from "react-navigation";
+import SendAlertScreen from "./SendAlert.js";
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Welcome"
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to SendAlertScreen"
+          onPress={() => this.props.navigation.navigate("SendAlert")}
+        />
+      </View>
+    );
+  }
+}
+
+const RootNav = StackNavigator(
+  {
+    Home: { screen: HomeScreen },
+    SendAlert: { screen: SendAlertScreen }
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Button title="HELP ME" color="#841584" accessibilityLabel="Please" />
-      </View>
-    );
+    return <RootNav />;
   }
 }
 
---
+++ SafeCheck/SendAlert.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { StackNavigator } from "react-navigation";
+
+class SendAlertScreen extends React.Component {
+  static navigationOptions = {
+    title: "Send Alert"
+  };
+  render() {
+    return <Text>Hi</Text>;
+  }
+}
+
+export default SendAlertScreen;
