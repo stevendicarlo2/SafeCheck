aa1e2cbd57267653e34039e1b678d549b2174a74
Ethan hodges <elh5wc@virginia.edu>
2018-03-24 21:31:18 -0400

    get alert info by phone number

---
+++ SafeCheck/InstitutionAlerts.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+  AsyncStorage
+} from "react-native";
+
+class InstitutionAlertScreen extends React.Component {
+  static navigationOptions = {
+    title: "Our Alerts"
+  };
+  constructor(props) {
+    super(props);
+    this.state = { data: "" };
+    console.log(this.state);
+  }
+  componentDidMount() {
+    this.loadData();
+  }
+  loadData() {
+    AsyncStorage.getItem("@MySuperStore:phoneNumber").then(phone => {
+      fetch("http://35.227.69.77/api/alert?phone=" + phone, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+        .then(response => response.json())
+        .then(responseJson => {
+          console.log(responseJson);
+          this.setState({
+            data: responseJson["alerts"][0]["id"]
+          });
+        });
+    });
+  }
+  render() {
+    console.log(this.state.data);
+    return <Text>{this.state.data}</Text>;
+  }
+}
+
+export default InstitutionAlertScreen;
--- SafeCheck/RootNav.js
+++ SafeCheck/RootNav.js
@@ -8,13 +8,15 @@ import {
 } from "react-native";
 import { StackNavigator } from "react-navigation";
 import SendAlertScreen from "./SendAlert.js";
+import InstitutionAlertScreen from "./InstitutionAlerts.js";
 
 const RootNav = StackNavigator(
   {
-    SendAlert: { screen: SendAlertScreen }
+    SendAlert: { screen: SendAlertScreen },
+    InstitutionAlerts: { screen: InstitutionAlertScreen }
   },
   {
-    initialRouteName: "SendAlert"
+    initialRouteName: "InstitutionAlerts"
   }
 );
 export default RootNav;
